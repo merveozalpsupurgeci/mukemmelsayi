427d8fe793088ea2dc5cd5239d6babe0bdd12a2d
merveozalpsupurgeci <92928158+merveozalpsupurgeci@users.noreply.github.com>
2023-12-23 02:47:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int toplam=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz :");
+        int n = input.nextInt();
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                toplam +=i;
+            }
+        }
+        if(toplam==n){
+            System.out.println(n+ " mükemmel sayıdır.");
+        }
+        else{
+            System.out.println(n+ " mükemmel sayı değildir.");
         }
     }
 }
